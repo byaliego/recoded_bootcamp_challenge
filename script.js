3a66ece08ce4fa7e8d0bf5c6632aaa7b81986990
Yigit Ali Ogun <62607936+byaliego@users.noreply.github.com>
2022-02-12 03:30:58 +0300

    Update script.js
    fixed fetch issue

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
-    let trypost = document.querySelector("#dataDisplay");
-    document.querySelector("#searchBtn").addEventListener("click",search());
-    let searchResult = document.querySelector("#searchResult");
-    
-    function search() {
-      
-    }
+let trypost = document.querySelector("#dataDisplay");
+document.querySelector("#searchBtn").addEventListener("click",search());
+let searchResult = document.querySelector("#searchResult");
+
+function search() {
+  
+}
 
 
-    fetch("data.json").then(
-      response => response.json()
-    ).then(responseJson => {
-      trypost.innerHTML = responseJson.countries + "<br>" + responseJson.bootcamps;
-    }
-    )
+fetch("data.json").then(
+  response => response.json()
+).then(responseJson => {
+  trypost.innerHTML = responseJson.countries + "<br>" + responseJson.bootcamps;
+}
+)
 
 
-    fetch("data.json").then(
-      response => response.json()
-    ).then(responseJson => {
-      console.log(responseJson.camps);
-      
-         searchResult.innerHTML = responseJson.camps.asd;
-      
-      
-    }
-    )
+fetch("data.json").then(
+  response => response.json()
+).then(responseJson => {
+  console.log(responseJson.camps);
+  
+     searchResult.innerHTML = responseJson.camps.asd;
+  
+}
+)
 
 
 
